d361537156ebb19207d3537851dba3ab8e78b4a4
Mongeese1 <60645887+Mongeese1@users.noreply.github.com>
2022-01-17 18:22:22 +0100

    Add files via upload

---
+++ crs/src/crs/CourseList.java
@@ -0,0 +1,14 @@
+package crs;
+
+
+
+public class CourseList {
+	public static void main(String... args) {
+	Course INT1001 = new Course("INT1001","xyz","phys",4,"AG");
+	System.out.println(INT1001.name);
+	Course INT1002 = new Course("Int1002","xxx","math",5,"EG");
+	}
+}
+
+
+
