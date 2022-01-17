5be43b76806eebed8c95a2f9625649529ef42674
BibiSernee <97899973+BibiSernee@users.noreply.github.com>
2022-01-17 18:07:45 +0100

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Course {
+
+    public String code;
+    public String name;
+    ArrayList <String> prereqs = new ArrayList<>();
+    ArrayList <String> coreqs = new ArrayList<>();
+    public String discipline;
+    public int period;
+    public String timeblock;
+
+    public Course(String code, String name, String discipline, int period, String timeblock ) {
+        this.code = code;
+        this.name = name;
+        this.discipline = discipline;
+        this.period = period;
+        this.timeblock = timeblock;
+    }
+
+    public void addPrereq(String code) {
+        prereqs.add(code);
+    }
+
+    public void addCoreq(String code) {
+        coreqs.add(code);
+    }
+}
+
+
